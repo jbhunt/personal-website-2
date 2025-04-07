c958118af8392abbb4258744a17940619b18c2dd
jbhunt <hunt.brian.joshua@gmail.com>
2025-04-06 19:17:29 -0600

    Switched from Plasmic Loader to Codegen

---
+++ components/plasmic/personal_website/icons/PlasmicIcon__MenuSvgrepoComSvg.jsx
@@ -0,0 +1,34 @@
+/* eslint-disable */
+/* tslint:disable */
+// @ts-nocheck
+/* prettier-ignore-start */
+import React from "react";
+import { classNames } from "@plasmicapp/react-web";
+
+export function MenuSvgrepoComSvgIcon(props) {
+  const { className, style, title, ...restProps } = props;
+  return (
+    <svg
+      xmlns={"http://www.w3.org/2000/svg"}
+      viewBox={"0 0 24 24"}
+      fill={"none"}
+      height={"1em"}
+      className={classNames("plasmic-default__svg", className)}
+      style={style}
+      {...restProps}
+    >
+      {title && <title>{title}</title>}
+
+      <path
+        d={"M4 6h16M4 12h16M4 18h16"}
+        stroke={"currentColor"}
+        strokeWidth={"2"}
+        strokeLinecap={"round"}
+        strokeLinejoin={"round"}
+      ></path>
+    </svg>
+  );
+}
+
+export default MenuSvgrepoComSvgIcon;
+/* prettier-ignore-end */
